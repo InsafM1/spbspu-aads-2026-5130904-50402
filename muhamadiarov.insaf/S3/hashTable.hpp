69b25e96f2aebdb753129a7b8700561f5df33374
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-05 09:58:30 +0300

    Made rehash and removed errors

--- muhamadiarov.insaf/S3/hashTable.hpp
+++ muhamadiarov.insaf/S3/hashTable.hpp
@@ -5,12 +5,19 @@
 
 namespace muhamadiarov
 {
+  enum class State
+  {
+    EMPTY,
+    OCCUPIED,
+    DELETE
+  };
+
   template < class Key, class Value >
   struct Slot
   {
     Key key_;
     Value value_;
-    bool empty_ = true;
+    State state_ = State::EMPTY;
     Slot() = default;
     Slot(const Key& key, const Value& value);
   };
@@ -33,9 +40,9 @@ namespace muhamadiarov
     void add(const Key& k, const Value& v);
     Value drop(const Key& k);
     bool has(const Key& k) const noexcept;
-    void rehesh(size_t slots);
-    bool empty() const noexcept;
+    void rehash(size_t countBucket, size_t newBucketCapacity);
     size_t size() const noexcept;
+    size_t capacity() const noexcept;
     size_t bucketCapacity() const noexcept;
     size_t bucketCount() const noexcept;
     size_t getBucketStart(const Key& k) const;
@@ -102,7 +109,7 @@ template < class Key, class Value >
 muh::Slot< Key, Value >::Slot(const Key& key, const Value& value):
   key_(key), 
   value_(value), 
-  empty_(false) 
+state_(State::OCCUPIED) 
 {}
 
 template < class Key, class Value, class Hash, class Equal >
@@ -135,7 +142,7 @@ muh::HashTable< Key, Value, Hash, Equal >::HashTable(const HashTable& other):
   {
     for (size_t i = 0; i < capacity_; ++i)
     {
-      if (!other.slots_[i].empty_)
+      if (other.slots_[i].state_ == State::EMPTY)
       {
         slots_[i] = other.slots_[i];
       }
@@ -223,7 +230,7 @@ muh::HashTable< Key, Value, Hash, Equal >::~HashTable() noexcept
 template < class Key, class Value, class Hash, class Equal >
 size_t muh::HashTable< Key, Value, Hash, Equal >::getBucketStart(const Key& k) const
 {
-  return hash_(k) % capacity_ * bucketCapacity_;
+  return (hash_(k) % bucketCount_) * bucketCapacity_;
 }
 
 template < class Key, class Value, class Hash, class Equal >
@@ -233,7 +240,11 @@ bool muh::HashTable< Key, Value, Hash, Equal >::findSLot(const Key& k, size_t& i
   size_t endBucket = startBucket + bucketCapacity_;
   for (size_t i = startBucket; i < endBucket; ++i)
   {
-    if (!slots_[i].empty && equal_(slots_[i].key_, k))
+    if (slots_[i].state_ == State::EMPTY)
+    {
+      return false;
+    }
+    if (slots_[i].state_ == State::OCCUPIED && equal_(slots_[i].key_, k))
     {
       id = i;
       return true;
@@ -244,13 +255,16 @@ bool muh::HashTable< Key, Value, Hash, Equal >::findSLot(const Key& k, size_t& i
   size_t endOtherflow = startOtherflow + bucketCapacity_;
   for (size_t i = startOtherflow; i < endOtherflow; ++i)
   {
-    if (!slots_[i].empty_ && equal(slots_[i].key_, k))
+    if (slots_[i].state_ == State::EMPTY)
+    {
+      return false;
+    }
+    if (slots_[i].state_ == State::OCCUPIED && equal(slots_[i].key_, k))
     {
       id = i;
       return true;
     }
   }
-
   return false;
 }
 
@@ -260,8 +274,8 @@ bool muh::HashTable< Key, Value, Hash, Equal >::findFreeSlot(const Key& k, size_
   size_t startBucket = getBucketStart(k);
   size_t endBucket = startBucket + bucketCapacity_;
   for (size_t i = startBucket; i < endBucket; ++i)
-  {
-    if (slots_[i].empty_)
+{
+    if (slots_[i].state_ != State::OCCUPIED)
     {
       id = i;
       return true;
@@ -270,34 +284,93 @@ bool muh::HashTable< Key, Value, Hash, Equal >::findFreeSlot(const Key& k, size_
 
   size_t startOtherflow = otherflowBucketIndex_;
   size_t endOtherflow = startOtherflow + bucketCapacity_;
-  for (size_t i = startBucket; i < endOtherflow; ++i)
+  for (size_t i = startOtherflow; i < endOtherflow; ++i)
   {
-    if (slots_[i].empty_)
+    if (slots_[i].state_ != State::OCCUPIED)
     {
       id = i;
       return true;
     }
   }
-
   return false;
 }
 
 template <class Key, class Value, class Hash, class Equal>
 void muh::HashTable< Key, Value, Hash, Equal >::add(const Key& k, const Value& v)
 {
-  size_t startBucket;
-  bool result = findSLot(k, startBucket);
+  size_t slotIndex;
+  bool result = findSLot(k, slotIndex);
   if (result)
   {
+    slots_[slotIndex].value_ = v;
     return;
   }
-  result = findFreeSlot(k, startBucket);
+  result = findFreeSlot(k, slotIndex);
   if (!result)
   {
     throw std::runtime_error("Hash-Table is otherflow: Not find free slot for data");
   }
-  slots_[startBucket] = Slot< Key, Value >(k, v);
+  slots_[slotIndex] = Slot< Key, Value >(k, v);
   ++size_;
 }
-  
+
+template <class Key, class Value, class Hash, class Equal>
+Value muh::HashTable< Key, Value, Hash, Equal >::drop(const Key& k)
+{
+  size_t slotIndex;
+  if (!findSLot(k, slotIndex))
+  {
+    throw std::out_of_range("Key not found");
+  }
+  Value val = slots_[slotIndex].value_;
+  slots_[slotIndex].state_ = State::DELETE;
+  --size_;
+  return val;
+}
+
+template <class Key, class Value, class Hash, class Equal>
+bool muh::HashTable< Key, Value, Hash, Equal >::has(const Key& k) const noexcept
+{
+  size_t dummy;
+  return findSLot(k, dummy);
+}
+
+template <class Key, class Value, class Hash, class Equal>
+void muh::HashTable< Key, Value, Hash, Equal >::rehash(size_t countBucket, size_t newBucketCapacity)
+{
+  HashTable newTable(newBucketCapacity, countBucket);
+  for (size_t i = 0; i < capacity_; ++i)
+  {
+    if (slots_[i].state_ == State::OCCUPIED)
+    {
+      newTable.add(slots_[i].key_, slots_[i].value_);
+    }
+  }
+
+  *this = std::move(newTable);
+}
+
+template <class Key, class Value, class Hash, class Equal>
+size_t muh::HashTable< Key, Value, Hash, Equal >::size() const noexcept
+{
+  return size_;
+}
+
+template <class Key, class Value, class Hash, class Equal>
+size_t muh::HashTable< Key, Value, Hash, Equal >::bucketCapacity() const noexcept
+{
+  return bucketCapacity_;
+}
+
+template <class Key, class Value, class Hash, class Equal>
+size_t muh::HashTable< Key, Value, Hash, Equal >::bucketCount() const noexcept
+{
+  return bucketCount_;
+}
+
+template <class Key, class Value, class Hash, class Equal>
+size_t muh::HashTable< Key, Value, Hash, Equal >::capacity() const noexcept
+{
+  return capacity_;
+}
 #endif
