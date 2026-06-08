a3e99d95bf691c9de3a6237d911052daf0ecbb74
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-08 21:22:15 +0300

    Add inbound and outbound

--- muhamadiarov.insaf/S3/commands.cpp
+++ muhamadiarov.insaf/S3/commands.cpp
@@ -3,6 +3,7 @@
 namespace muhamadiarov
 {
   using HTIter_t = HTIter< std::string, Graph, SipHash<std::string>, std::equal_to<std::string> >;  
+  using HTIter_k_t = HTIter< Graph::key_t, List< size_t >, PairHash, std::equal_to< Graph::key_t > >;
 
   void sortStringList(List< std::string >& list)
   {
@@ -106,7 +107,7 @@ namespace muhamadiarov
     in >> graphName;
     if (!graphs.has(graphName))
     {
-      out << "<INVALID COMMAND>";
+      out << "<INVALID COMMAND>\n";
       return;
     }
     const Graph& graph = graphs.cget(graphName);
@@ -121,6 +122,123 @@ namespace muhamadiarov
     while (printIt != vertices.cend())
     {
       out << *printIt << '\n';
+      ++printIt;
+    }
+  }
+
+  void cmdOutbound(std::istream& in, std::ostream& out, GraphTable& graphs)
+  {
+    std::string graphName;
+    std::string vertex;
+    in >> graphName >> vertex;
+    if (!graphs.has(graphName))
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+    Graph& graph = graphs.get(graphName);
+    if (!graph.findV(vertex))
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+    List< std::pair< std::string, List< size_t > > > outbound;
+    HTIter_k_t it = graph.bonds_.begin();
+    while (it != graph.bonds_.end())
+    {
+      const Graph::key_t& key = it->key_;
+      if (key.second == vertex)
+      {
+        std::pair< std::string, List< size_t > > item;
+        item.first = key.first;
+        List< size_t > weights;
+        LCIter<size_t> wIt = it->value_.cbegin();
+        while (wIt != it->value_.cend())
+        {
+          weights.pushBack(*wIt);
+          ++wIt;
+        }
+
+        sortWeights(weights);
+        item.second = weights;
+        outbound.pushBack(item);
+      }
+      ++it;
+    }
+    sortOutboundList(outbound);
+    LIter<std::pair<std::string, List<size_t> > > printIt = outbound.begin();
+    while (printIt != outbound.end())
+    {
+      out << printIt->first;
+      LCIter<size_t> wIt = printIt->second.cbegin();
+      while (wIt != printIt->second.cend())
+      {
+        out << " " << *wIt;
+        ++wIt;
+      }
+      out << "\n";
+      ++printIt;
+    }
+  }
+
+  void cmdInbound(std::istream& in, std::ostream& out, GraphTable& graphs)
+  {
+    std::string graphName;
+    std::string vertex;
+    in >> graphName >> vertex;
+
+    if (!graphs.has(graphName))
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+
+    Graph& graph = graphs.get(graphName);
+    if (!graph.findV(vertex))
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+
+    List<std::pair<std::string, List<size_t> > > inbound;
+
+    HTIter_k_t it = graph.bonds_.begin();
+    while (it != graph.bonds_.end())
+    {
+      const Graph::key_t& key = it->key_;
+      if (key.second == vertex)
+      {
+        std::pair<std::string, List<size_t> > item;
+        item.first = key.first;
+
+        List<size_t> weights;
+        LCIter<size_t> wIt = it->value_.cbegin();
+        while (wIt != it->value_.cend())
+        {
+          weights.pushBack(*wIt);
+          ++wIt;
+        }
+
+        sortWeights(weights);
+        item.second = weights;
+        inbound.pushBack(item);
+      }
+      ++it;
+    }
+
+    sortOutboundList(inbound);
+
+    LIter<std::pair<std::string, List<size_t> > > printIt = inbound.begin();
+    while (printIt != inbound.end())
+    {
+      out << printIt->first;
+      LCIter<size_t> wIt = printIt->second.cbegin();
+      while (wIt != printIt->second.cend())
+      {
+        out << " " << *wIt;
+        ++wIt;
+      }
+      out << "\n";
       ++printIt;
     }
   }
