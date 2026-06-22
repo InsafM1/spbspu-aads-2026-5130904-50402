2d010bf40ec288c1debb1a5432b3acd8d554f81b
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-23 00:45:27 +0300

    Done robot and edge

---
+++ muhamadiarov.insaf/F0/robot.hpp
@@ -0,0 +1,75 @@
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+#include <algorithm>
+
+namespace muhamadiarov
+{
+  struct Robot {
+    double maxEnergy_;
+    double recoveryRate_;
+    double speed_;
+    double currentEnergy_;
+    
+    Robot() = delete;
+    
+    Robot(double e, double r, double s):
+      maxEnergy_(e),
+      recoveryRate_(r),
+      speed_(s),
+      currentEnergy_(e) {}
+    
+    void resetEnergy()
+    { 
+      currentEnergy_ = maxEnergy_; 
+    }
+    
+    void charge()
+    { 
+      currentEnergy_ = std::min(maxEnergy_, currentEnergy_ + recoveryRate_); 
+    }
+    
+    bool consumeEnergy(double amount)
+    {
+      if (currentEnergy_ >= amount)
+      {
+        currentEnergy_ -= amount;
+        return true;
+      }
+      return false;
+    }
+    
+    double getMaxEnergy() const
+    {
+      return maxEnergy_;
+    }
+    double getRecoveryRate() const
+    {
+      return recoveryRate_;
+    }
+    double getSpeed() const
+    {
+      return speed_;
+    }
+    double getCurrentEnergy() const
+    {
+      return currentEnergy_;
+    }
+    
+    void setMaxEnergy(double e)
+    { 
+      maxEnergy_ = e; 
+      currentEnergy_ = std::min(currentEnergy_, maxEnergy_);
+    }
+    
+    void setRecoveryRate(double r)
+    {
+      recoveryRate_ = r;
+    }
+    void setSpeed(double s)
+    {
+      speed_ = s;
+    }
+  };
+}
+
+#endif
