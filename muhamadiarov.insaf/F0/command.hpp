0d9a2d3fe4a6ffddcedbe44316466b15a77466cd
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-23 12:12:42 +0300

    Create command for user

---
+++ muhamadiarov.insaf/F0/command.hpp
@@ -0,0 +1,26 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <string>
+#include "graph.hpp"
+#include "hashtable.hpp"
+
+namespace muhamadiarov
+{
+  using GraphTable = RobinTable< std::string, Graph, SipHash< std::string >, std::equal_to< std::string > >;
+
+  void makeLandscape(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void makeRobot(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void setRobot(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void addEdge(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void addVertex(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void rmVertex(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void rmEdge(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void setVertex(std::istream& in, std::ostream& out, GraphTable& graphs); 
+  void setVertex(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void setEdge(std::istream& in, std::ostream& out, GraphTable& graphs);
+  Graph findPath(std::istream& in, std::ostream& out, GraphTable& graphs);
+  Graph findGold(std::istream& in, std::ostream& out, GraphTable& graphs);
+  Graph findVisitall(std::istream& in, std::ostream& out, GraphTable& graphs);
+  Graph findFastes(std::istream& in, std::ostream& out, GraphTable& graphs);
+}
+#endif
